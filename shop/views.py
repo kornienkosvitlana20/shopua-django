9352ee3801ee61f652fdd9d7c915bd97aa9b8251
vialkovapolina07-jpg <vialkovapolina07@gmail.com>
2026-05-23 01:07:11 +0300

    docs: додано інформацію про автора views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+# Views розроблені: Вялкова Поліна 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
